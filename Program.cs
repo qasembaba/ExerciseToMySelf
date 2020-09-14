8d454a88ec0c90133ffa5d77b9509b635bb82a97
Qababa1000 <deltagare@LEXJONKAF07>
2020-09-14 15:11:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExerciseToMySelf
+{
+    class Program
+    {
+        private static readonly IEnumerable<object> multiTabel;
+
+        public static int[,] MultiTable { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" hello world");
+        }
+
+
+        static void Arrays()
+        {
+            int[,] multiTable = new int[10, 10];
+
+            foreach ( var item in multiTabel)
+            {
+                Console.WriteLine(item);
+            }
+
+            SetMultiNumbers(MultiTable);
+
+            Console.WriteLine("after method call ");
+
+            foreach (var item in multiTabel)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+        static void SetMultiNumbers (int [,] tabel)
+        {
+            for (int i = 0;  i <=  10; i++  )
+            {
+                for (int x =0; x < 10; x++)
+                {
+                    tabel[i, x] = (i + 1) * (x + 1);
+                }
+            }
+        }
+    }
+}
